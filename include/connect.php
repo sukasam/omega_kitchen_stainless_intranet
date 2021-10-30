350cb8afff50e57a604bbe9872e2e236c0a90851
Dumrus.S <dumrus.sukasam@unicity.com>
2021-10-30 09:55:57 +0700

    Updated connect database

--- include/connect.php
+++ include/connect.php
@@ -1,5 +1,5 @@
 <?php     
-	$conn = mysqli_connect("localhost", "root", "", "omega_kitchen_intranet");
+	$conn = mysqli_connect("localhost", "root", "", "omegaint_kitchen");
 	@mysqli_query($conn,"SET NAMES UTF8");
 
 //        $conn = mysqli_connect("localhost", "omegaint_kitchen", "oNh0h9QzlS", "omegaint_kitchen");
